5f4ef8c2015a7e0e1f0f975f2a33a17e68bc0b4f
Ryan Fox <ryan.fox@altium.com>
2020-05-10 08:13:04 -0400

    Add comment block at the top.

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import dialogs
 
 if __name__ == '__main__':
     with boatlib.data.collect_records() as c:
+        boatlib.data.Comment('''
+        Horizon's Gate Farming Mod
+        By: rcfox (https://github.com/rcfox/HorizonsGateFarmMod)
+        Crop sprites by: josehzz (https://opengameart.org/content/farming-crops-16x16)
+        ''')
         tools.define_tools()
         plants.define_plants()
         dialogs.define_dialogs()
